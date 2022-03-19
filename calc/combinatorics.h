dab9f7f071c228b472df63303c85890780d8961d
Тима Газдиев <gtrx32@bk.ru>
2022-03-19 23:58:51 +0300

    калькулятор комбинаторики

--- calc/combinatorics.h
+++ calc/combinatorics.h
@@ -1,2 +1,15 @@
 #pragma once
+#include <iostream>
+#include "ChekOnRightOfNumber.h"
+
+void PlacementRepeat(); // Размещение с повторениями
+
+void PlacementNoRepeat(); // Размещение без повторений
+
+void CombinationRepeat(); // Сочетание с повторениями
+
+void CombinationNoRepeat(); // Сочетание без повторений
+
+void Transposition(); // Перестановка (без повторений)
+
 void combinatorics(int taskNumber);
